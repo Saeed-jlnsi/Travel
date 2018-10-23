afef410471bde5c5e2e7c93b916467f6267fef7d
saeed-jlnsi <saeed.jalinoosi@gmail.com>
2018-10-23 18:07:18 +0330

    Footer initial styles and donwload social media icons

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,7 +1,20 @@
 <template>
     <div :class="$style.footer">
-        <div :class="footerLinks">
-            <Menu :menu="menu"></Menu>
+        <div :class="$style.innerFooter">
+            <div :class="$style.footerLinks">
+                <Menu :menu="menu"></Menu>
+            </div>
+            <div :class="$style.socialMedia">
+                <a href="#"><img :class="$style.socialMediaIcon" src="../../assets/image/facebook.svg"></a>
+                <a href="#"><img :class="$style.socialMediaIcon" src="../../assets/image/instagram.svg"></a>
+                <a href="#"><img :class="$style.socialMediaIcon" src="../../assets/image/telegram.svg"></a>
+                <a href="#"><img :class="$style.socialMediaIcon" src="../../assets/image/twitter.svg"></a>
+                <a href="#"><img :class="$style.socialMediaIcon" src="../../assets/image/linkedin.svg"></a>
+            </div>
+            <div :class="$style.copyRight">
+                <p>Travel To Kish © 2018</p>
+            </div>
+            
         </div>
     </div>
 </template>
