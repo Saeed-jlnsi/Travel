0a54bf4c7089635a699c5c1538b7d3c83c34358b
saeed-jlnsi <saeed.jalinoosi@gmail.com>
2019-07-06 23:59:26 +0430

    Try to REFACTORE the FOOTER(still need work)

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,45 +2,40 @@
   <header :class="$style.header">
     <div>
       <div :class="$style.logo">
-        <img :src="imageLink" alt="travel to kish">
+        <img :src="imageLink" alt="travel to kish" />
       </div>
     </div>
-      <div :class="$style.navBar">
-        <Menu :menu="menu"></Menu>
-      </div>
-      <!-- <div :class="$style.sidebar">
+    <div :class="$style.navBar">
+      <!-- <Menu :menu="menu"></Menu> -->
+    </div>
+    <!-- <div :class="$style.sidebar">
         <div :class="$style.mobileNavIcon" v-on:click.prevent="isMenuOpened = !isMenuOpened">
           <img src="../../assets/image/phone-nav.svg" alt="">
         </div>
         <div :class="$style.mobileNav" v-if="isMenuOpened">
           <Menu :menu="menu"></Menu>
         </div>
-      </div> -->
+    </div>-->
   </header>
 </template>
 
 <script>
-import Menu from '@/components/menu/Menu.vue'
+import Menu from "@/components/menu/Menu.vue";
 
 export default {
   data() {
     return {
-      menu:[
-        'home' ,
-        'about KISH',
-        'contact us'
-      ],
-      imageLink: require('@/assets/image/logo.png'),
+      menu: ["home", "about KISH", "contact us"],
+      imageLink: require("@/assets/image/logo.png"),
       isMenuOpened: false
-
-    }
+    };
   },
-  components : {
+  components: {
     Menu
   }
-}
+};
 </script>
 
 <style lang="scss" module>
-  @import './header';
+@import "./header";
 </style>
